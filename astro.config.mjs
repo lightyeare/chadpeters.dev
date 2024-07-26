aa71438da37ef560ebb0beae7d3f087dc9f42c31
Chad Peters <chad.peters@ritterim.com>
2024-07-26 13:02:29 -0400

    Update resources for blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
+  site: 'https://chadpeters.dev',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
